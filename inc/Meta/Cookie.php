cc27e688603476d1b17449cbc94880e3ea367971
badasswp <badasswpdev@gmail.com>
2025-08-16 18:58:27 +0100

    chore: rename meta labels

--- inc/Meta/Cookie.php
+++ inc/Meta/Cookie.php
@@ -39,14 +39,14 @@ class Cookie extends Meta {
 
 		return [
 			'ccal_cookie_id'     => [
-				'label'     => esc_html__( 'ID', 'cookie-consent-and-logging' ),
+				'label'     => esc_html__( 'Cookie ID', 'cookie-consent-and-logging' ),
 				'value'     => get_post_meta( $post_id, 'ccal_cookie_id', true ),
 				'type'      => 'string',
 				'default'   => '',
 				'rest_name' => 'ccalCookieId',
 			],
 			'ccal_cookie_domain' => [
-				'label'     => esc_html__( 'Domain', 'cookie-consent-and-logging' ),
+				'label'     => esc_html__( 'Cookie Domain', 'cookie-consent-and-logging' ),
 				'value'     => get_post_meta( $post_id, 'ccal_cookie_domain', true ),
 				'type'      => 'string',
 				'default'   => '',
