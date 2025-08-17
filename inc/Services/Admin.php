20abc3433a191b10b3612a4a97f955d92586df29
badasswp <badasswpdev@gmail.com>
2025-08-17 16:04:08 +0100

    feat: implement admin options page

--- inc/Services/Admin.php
+++ inc/Services/Admin.php
@@ -37,6 +37,55 @@ class Admin extends Service implements Kernel {
 	const PLUGIN_GROUP = 'cookie-consent-and-logging-group';
 
 	/**
+	 * Plugin Section.
+	 *
+	 * @var string
+	 */
+	const PLUGIN_SECTION = 'cookie-consent-and-logging-section';
+
+	/**
+	 * Notice Title.
+	 *
+	 * @var string
+	 */
+	const CCAL_TITLE = 'title';
+
+	/**
+	 * Notice Content.
+	 *
+	 * @var string
+	 */
+	const CCAL_CONTENT = 'content';
+
+	/**
+	 * Notice Button (Yes).
+	 *
+	 * @var string
+	 */
+	const CCAL_YES_BTN = 'yes_btn';
+
+	/**
+	 * Notice Button (No).
+	 *
+	 * @var string
+	 */
+	const CCAL_NO_BTN = 'no_btn';
+
+	/**
+	 * Notice Button (Preferences).
+	 *
+	 * @var string
+	 */
+	const CCAL_PREF_BTN = 'pref_btn';
+
+	/**
+	 * Enable Cookie Banner.
+	 *
+	 * @var string
+	 */
+	const CCAL_ENABLE = 'enable';
+
+	/**
 	 * Plugin Options.
 	 *
 	 * @var mixed[]
@@ -52,6 +101,7 @@ class Admin extends Service implements Kernel {
 	 */
 	public function register(): void {
 		add_action( 'admin_menu', [ $this, 'register_options_page' ] );
+		add_action( 'admin_init', [ $this, 'register_options_init' ] );
 	}
 
 	/**
@@ -104,5 +154,325 @@ class Admin extends Service implements Kernel {
 			</form>
 		</div>
 		<?php
+	}
+
+	/**
+	 * Register Options Init.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return void
+	 */
+	public function register_options_init(): void {
+		register_setting(
+			self::PLUGIN_GROUP,
+			self::PLUGIN_OPTION,
+			[ $this, 'sanitize_options' ]
+		);
+
+		foreach ( $this->get_sections() as $section ) {
+			add_settings_section(
+				$section['name'] ?? '',
+				$section['label'] ?? '',
+				null,
+				self::PLUGIN_SLUG
+			);
+		}
+
+		foreach ( $this->get_options() as $option ) {
+			if ( ! isset( $option['name'] ) || ! isset( $option['cb'] ) || ! is_callable( $option['cb'] ) ) {
+				continue;
+			}
+
+			add_settings_field(
+				$option['name'] ?? '',
+				$option['label'] ?? '',
+				$option['cb'],
+				$option['page'] ?? '',
+				$option['section'] ?? ''
+			);
+		}
+	}
+
+	/**
+	 * Get Form Sections.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return mixed[]
+	 */
+	protected function get_sections(): array {
+		return [
+			[
+				'name'  => self::PLUGIN_SECTION,
+				'label' => esc_html__( 'Settings', 'cookie-and-consent-logging' ),
+			],
+		];
+	}
+
+	/**
+	 * Get Callback name.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param string $name Form Control name.
+	 * @return string
+	 */
+	protected function get_callback_name( $name ): string {
+		return sprintf( '%s_cb', $name );
+	}
+
+	/**
+	 * Get Plugin Options.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return mixed[]
+	 */
+	protected function get_options(): array {
+		$options = [
+			[
+				'name'    => self::CCAL_ENABLE,
+				'label'   => esc_html__( 'Enable Cookie Banner', 'cookie-and-consent-logging' ),
+				'cb'      => [ $this, $this->get_callback_name( self::CCAL_ENABLE ) ],
+				'page'    => self::PLUGIN_SLUG,
+				'section' => self::PLUGIN_SECTION,
+			],
+			[
+				'name'    => self::CCAL_TITLE,
+				'label'   => esc_html__( 'Banner Title', 'cookie-and-consent-logging' ),
+				'cb'      => [ $this, $this->get_callback_name( self::CCAL_TITLE ) ],
+				'page'    => self::PLUGIN_SLUG,
+				'section' => self::PLUGIN_SECTION,
+			],
+			[
+				'name'    => self::CCAL_CONTENT,
+				'label'   => esc_html__( 'Banner Content', 'cookie-and-consent-logging' ),
+				'cb'      => [ $this, $this->get_callback_name( self::CCAL_CONTENT ) ],
+				'page'    => self::PLUGIN_SLUG,
+				'section' => self::PLUGIN_SECTION,
+			],
+			[
+				'name'    => self::CCAL_YES_BTN,
+				'label'   => esc_html__( 'Button Label (Yes)', 'cookie-and-consent-logging' ),
+				'cb'      => [ $this, $this->get_callback_name( self::CCAL_YES_BTN ) ],
+				'page'    => self::PLUGIN_SLUG,
+				'section' => self::PLUGIN_SECTION,
+			],
+			[
+				'name'    => self::CCAL_NO_BTN,
+				'label'   => esc_html__( 'Button Label (No)', 'cookie-and-consent-logging' ),
+				'cb'      => [ $this, $this->get_callback_name( self::CCAL_NO_BTN ) ],
+				'page'    => self::PLUGIN_SLUG,
+				'section' => self::PLUGIN_SECTION,
+			],
+			[
+				'name'    => self::CCAL_PREF_BTN,
+				'label'   => esc_html__( 'Button Label (Preferences)', 'cookie-and-consent-logging' ),
+				'cb'      => [ $this, $this->get_callback_name( self::CCAL_PREF_BTN ) ],
+				'page'    => self::PLUGIN_SLUG,
+				'section' => self::PLUGIN_SECTION,
+			],
+		];
+
+		/**
+		 * Filter Option Fields.
+		 *
+		 * @since 1.0.0
+		 *
+		 * @param mixed[] $options Option Fields.
+		 * @return mixed[]
+		 */
+		return apply_filters( 'cookie_and_consent_logging_admin_fields', $options );
+	}
+
+	/**
+	 * Enable Banner Callback.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return void
+	 */
+	public function enable_cb(): void {
+		printf(
+			'<input
+				type="checkbox"
+				id="%2$s"
+				name="%1$s[%2$s]"
+				value="1" %3$s
+			/>',
+			esc_attr( self::PLUGIN_OPTION ),
+			esc_attr( self::CCAL_ENABLE ),
+			checked( 1, $this->options[ self::CCAL_ENABLE ] ?? 0, false )
+		);
+	}
+
+	/**
+	 * Title Callback.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return void
+	 */
+	public function title_cb(): void {
+		printf(
+			'<input
+			   type="text"
+			   id="%2$s"
+			   name="%1$s[%2$s]"
+			   placeholder="%4$s"
+			   value="%3$s"
+			   class="wide"
+		   />',
+			esc_attr( self::PLUGIN_OPTION ),
+			esc_attr( self::CCAL_TITLE ),
+			esc_attr( $this->options[ self::CCAL_TITLE ] ?? '' ),
+			esc_html__( 'Cookies Consent', 'cookie-consent-and-logging' )
+		);
+	}
+
+	/**
+	 * Content Callback.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return void
+	 */
+	public function content_cb(): void {
+		printf(
+			'<textarea
+				id="%2$s"
+				name="%1$s[%2$s]"
+				rows="5"
+				cols="50"
+				placeholder="%4$s"
+			>%3$s</textarea>',
+			esc_attr( self::PLUGIN_OPTION ),
+			esc_attr( self::CCAL_CONTENT ),
+			esc_attr( $this->options[ self::CCAL_CONTENT ] ?? '' ),
+			esc_html__( 'To provide the best experiences, we use cookies to store and/or access device information. Consenting to these technologies will allow us to process data such as browsing behavior or unique IDs on this site.', 'cookie-consent-and-logging' )
+		);
+	}
+
+	/**
+	 * Yes Button Callback.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return void
+	 */
+	public function yes_btn_cb(): void {
+		printf(
+			'<input
+			   type="text"
+			   id="%2$s"
+			   name="%1$s[%2$s]"
+			   placeholder="%4$s"
+			   value="%3$s"
+			   class="wide"
+		   />',
+			esc_attr( self::PLUGIN_OPTION ),
+			esc_attr( self::CCAL_YES_BTN ),
+			esc_attr( $this->options[ self::CCAL_YES_BTN ] ?? '' ),
+			esc_html__( 'Accept (Yes)', 'cookie-consent-and-logging' )
+		);
+	}
+
+	/**
+	 * No Button Callback.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return void
+	 */
+	public function no_btn_cb(): void {
+		printf(
+			'<input
+			   type="text"
+			   id="%2$s"
+			   name="%1$s[%2$s]"
+			   placeholder="%4$s"
+			   value="%3$s"
+			   class="wide"
+		   />',
+			esc_attr( self::PLUGIN_OPTION ),
+			esc_attr( self::CCAL_NO_BTN ),
+			esc_attr( $this->options[ self::CCAL_NO_BTN ] ?? '' ),
+			esc_html__( 'Reject (No)', 'cookie-consent-and-logging' )
+		);
+	}
+
+	/**
+	 * Preferences Button Callback.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return void
+	 */
+	public function pref_btn_cb(): void {
+		printf(
+			'<input
+			   type="text"
+			   id="%2$s"
+			   name="%1$s[%2$s]"
+			   placeholder="%4$s"
+			   value="%3$s"
+			   class="wide"
+		   />',
+			esc_attr( self::PLUGIN_OPTION ),
+			esc_attr( self::CCAL_PREF_BTN ),
+			esc_attr( $this->options[ self::CCAL_PREF_BTN ] ?? '' ),
+			esc_html__( 'Manage Preferences', 'cookie-consent-and-logging' )
+		);
+	}
+
+	/**
+	 * Sanitize Options.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param mixed[] $input Plugin Options.
+	 * @return mixed[]
+	 */
+	public function sanitize_options( $input ): array {
+		$sanitized_options = [];
+
+		if ( isset( $input[ self::CCAL_ENABLE ] ) ) {
+			$input_data = trim( (string) $input[ self::CCAL_ENABLE ] );
+
+			$sanitized_options[ self::CCAL_ENABLE ] = absint( $input_data );
+		}
+
+		if ( isset( $input[ self::CCAL_TITLE ] ) ) {
+			$input_data = trim( (string) $input[ self::CCAL_TITLE ] );
+
+			$sanitized_options[ self::CCAL_TITLE ] = sanitize_text_field( $input_data );
+		}
+
+		if ( isset( $input[ self::CCAL_CONTENT ] ) ) {
+			$input_data = trim( (string) $input[ self::CCAL_CONTENT ] );
+
+			$sanitized_options[ self::CCAL_CONTENT ] = sanitize_textarea_field( $input_data );
+		}
+
+		if ( isset( $input[ self::CCAL_YES_BTN ] ) ) {
+			$input_data = trim( (string) $input[ self::CCAL_YES_BTN ] );
+
+			$sanitized_options[ self::CCAL_YES_BTN ] = sanitize_text_field( $input_data );
+		}
+
+		if ( isset( $input[ self::CCAL_NO_BTN ] ) ) {
+			$input_data = trim( (string) $input[ self::CCAL_NO_BTN ] );
+
+			$sanitized_options[ self::CCAL_NO_BTN ] = sanitize_text_field( $input_data );
+		}
+
+		if ( isset( $input[ self::CCAL_PREF_BTN ] ) ) {
+			$input_data = trim( (string) $input[ self::CCAL_PREF_BTN ] );
+
+			$sanitized_options[ self::CCAL_PREF_BTN ] = sanitize_text_field( $input_data );
+		}
+
+		return $sanitized_options;
 	}
 }
